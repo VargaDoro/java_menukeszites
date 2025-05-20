399bbf9a188dfa1943e3aad168b614c9141887be
VargaDoro <varga.dorothea.timea@gmail.com>
2025-05-20 08:57:51 +0200

    modell class Konfiguracio

---
+++ src/modell/Konfiguracio.java
@@ -0,0 +1,36 @@
+package modell;
+
+public class Konfiguracio {
+    //ADATTAGOK
+    private String nev;
+    private int szakIndex;
+    private boolean hirlevel;
+    
+    //GENERÁLHATÓAK: ALT + INSERT
+    //TAGFV.
+
+    public Konfiguracio(String nev, int szakIndex, boolean hirlevel) {
+        this.nev = nev;
+        this.szakIndex = szakIndex;
+        this.hirlevel = hirlevel;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public int getSzakIndex() {
+        return szakIndex;
+    }
+
+    public boolean isHirlevel() {
+        return hirlevel;
+    }
+
+    //@Override //öröklés az Obj osztályból - felülírjuk a metódust - az obj szöveges reprezentációja
+    public String toString() {
+        return "Konfiguracio{" + "nev=" + nev + ", szakIndex=" + szakIndex + ", hirlevel=" + hirlevel + '}';
+    }
+    
+    
+}
